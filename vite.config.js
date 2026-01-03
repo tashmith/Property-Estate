c1099192a3f83aa8b9dcc5c36be36fd311388e70
Mesith Bopage <mesithbopage@gmail.com>
2026-01-03 15:15:41 +0530

    Configure testing in Vite

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Property-Estate/',  
-
   plugins: [react()],
   resolve: {
     alias: {
